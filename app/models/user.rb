145a2b602e5a739c2a41720041e78daac73d2678
sanohitomi800 <h.tommy.0830s@gmail.com>
2021-04-08 12:38:45 +0900

    usersモデルバリデーション完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+    
+         validates :nickname, presence:true
+         validates :last_name, presence: true
+         validates :fast_name, presence: true
+         validates :last_name_kana, presence: true
+         validates :fast_name_kana, presence: true
+         validates :birthday, presence: true     
 end
