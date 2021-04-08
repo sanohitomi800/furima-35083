39fdba8ecfaa52dc5fc1debb3d643293bb633218
sanohitomi800 <h.tommy.0830s@gmail.com>
2021-04-08 15:27:20 +0900

    ユーザー新規登録の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  
+    private
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :fast_name, :last_name_kana, :fast_name_kana, :birthday])
+    end
+  
 end
