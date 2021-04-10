6103fb96f56d72844444d4dd2ab77c5220be6013
sanohitomi800 <h.tommy.0830s@gmail.com>
2021-04-10 22:34:48 +0900

    ActiveStorage導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,17 +1,7 @@
 class Item < ApplicationRecord
 
-  with_options presence: true do 
-    validates :name
-    validates :info
-    validates :category_id
-    validates :status_id
-    validates :shipping_fee_id
-    validates :prefecture_id
-    validates :days_id
-    validates :price 
-  end
-
     belongs_to :user
-    #has_one :purchase
+     #has_one :purchase
+    has_one_attached :image
 
 end
