fcbd36ccae5b2386f64954459e07d811fd595173
sanohitomi800 <h.tommy.0830s@gmail.com>
2021-04-13 17:02:29 +0900

    販売手数料.販売利益の計算結果が表示

---
+++ app/javascript/item_price.js
@@ -0,0 +1,14 @@
+function price (){
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+    
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1)
+    
+    const profitNumber = document.getElementById("profit");
+    profitNumber.innerHTML = Math.floor(inputValue - (inputValue * 0.1))
+  });
+}
+window.addEventListener('load', price)
