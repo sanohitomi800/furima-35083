207f9c74060562aad16f72b3d746169767929f06
sanohitomi800 <h.tommy.0830s@gmail.com>
2021-04-20 19:06:29 +0900

    クレジットカード決済完了

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,13 +1,35 @@
 class PurchasesController < ApplicationController
 
-def index
-@purchase_address = PurchaseAddress.new(@item_params) #フォームオブジェクトのインスタンスを生成し、インスタンス変数に代入する
-@item = Item.find(params[:item_id])
+  def index
+   @purchase_address = PurchaseAddress.new(@item_params) #フォームオブジェクトのインスタンスを生成し、インスタンス変数に代入する
+  @item = Item.find(params[:item_id])
+  end
+
+  def create 
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    @item = Item.find(params[:item_id])
+    
+    if  @purchase_address.valid?
+     pay_item
+     @purchase_address.save
+     redirect_to root_path
+    else
+      render :index   
+    end
+  end
+
+  private
+
+    def purchase_params
+    params.permit(:postalcoad, :prefecture_id, :city, :apartment, :addresses, :phon_number, :item_id, :token).merge( user_id: current_user.id)  
+    end
+    def pay_item
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"] 
+      Payjp::Charge.create(  
+        amount: @item.price,            
+        card: @purchase_address.token,  
+        currency: 'jpy'                 
+          ) 
+    end
 end
 
-def new
-
-end
-
-
-end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,6 +1,6 @@
 class PurchaseAddress 
 include ActiveModel::Model
-attr_accessor :postalcoad, :prefecture_id, :city, :addresses, :apartment, :phon_number, :purchase_id, :user_id, :item_id
+attr_accessor :postalcoad, :prefecture_id, :city, :addresses, :apartment, :phon_number,:user_id, :item_id, :token
 
 with_options presence: true do
   validates :postalcoad, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -8,16 +8,15 @@ with_options presence: true do
   validates :city
   validates :addresses
   validates :phon_number, format: {with: /\A\d{11}\z/}
-  validates :purchase_id
   validates :user_id
   validates :item_id
-  
+  validates :token
 end
+
 def save 
-#誰がいつどの情報を購入したのか記録するテーブル
-@purchase = Purchase.creat(user_id: user_id, item_id: item_id )#
-#住所を保存する
-ADDress.create(postalcoad: postalcoad, prefecture_id: prefecture_id, city: city, addresses: addresses, phon_number: phon_number, purchase_id: purchase_id)
+@purchase = Purchase.create(user_id: user_id, item_id: item_id )
+
+Address.create(apartment: apartment, postalcoad: postalcoad, prefecture_id: prefecture_id, city: city, addresses: addresses, phon_number: phon_number, purchase_id: @purchase.id)
 end
 
 
