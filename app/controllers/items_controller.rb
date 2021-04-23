0bf73efb1129053f378c45f66c14e161293c5e9a
sanohitomi800 <h.tommy.0830s@gmail.com>
2021-04-23 12:44:31 +0900

    修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -54,8 +54,8 @@ class ItemsController < ApplicationController
   end
   
   def root_item
-    if @item.user_id ！= current_user.id || @item.purchase.present?
-        redirect_to root_path
+    if @item.user_id != current_user.id || @item.purchase.present?
+      redirect_to root_path
     end
   end
 end
